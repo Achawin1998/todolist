2ebe8f936965fe92000776ab1af637d051515220
achawin <achawin1998s_@hotmail.com>
2024-07-03 07:46:09 +0700

    update-complete

--- app/components/DelBtn.jsx
+++ app/components/DelBtn.jsx
@@ -5,7 +5,7 @@ import  {AiFillDelete} from "react-icons/ai"
 function DelBtn({ item , onDelete }) {
   return (
     <button className='hover:scale-110'><AiFillDelete size={25} style={{color: 'red'}} onClick={() => {
-        const confrimDel = window.confirm(`Are u sure to delete ${item.data.title} list ?`)
+        const confrimDel = window.confirm(`Are you sure to delete ${item.data.title} list ?`)
         if (confrimDel) {
           onDelete(item.id)
         }
